99cbc67b0f8e3c3ade0c72f1fd1d37557ec78749
tympush <tymur.pushnoy@gmail.com>
2024-09-27 12:50:42 +0200

    save before trying to animate hamburger

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -13,8 +13,8 @@ const currentPage = window.location.pathname.split("/").pop();
 const navLinks = document.querySelectorAll('.nav-link');
 
 navLinks.forEach(link => {
-  const linkPage = link.getAttribute('href').split("/").pop();
-  if (linkPage === currentPage) {
-    link.classList.add('active');
-  }
+    const linkPage = link.getAttribute('href').split("/").pop();
+    if (linkPage === currentPage) {
+        link.classList.add('active');
+    }
 });
