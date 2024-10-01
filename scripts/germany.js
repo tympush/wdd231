d30247fed70eb9840c54b6b389d42532b63450d0
tympush <tymur.pushnoy@gmail.com>
2024-10-01 17:37:14 +0200

    fixed germany

--- scripts/germany.js
+++ scripts/germany.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/3.0/?lat={49.75}&lon={6.64}&units=metric&appid={737eba97a69e37d9ea630a80b842a86f}';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=metric&appid=737eba97a69e37d9ea630a80b842a86f';
 
 async function apiFetch() {
     try {
@@ -10,7 +10,7 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
-
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -18,5 +18,15 @@ async function apiFetch() {
         console.log(error);
     }
 }
-  
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+// Call apiFetch to run the code
 apiFetch();
