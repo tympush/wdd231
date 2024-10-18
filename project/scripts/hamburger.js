767ef70886475b467daa9add1442a28c06fcc620
tympush <tymur.pushnoy@gmail.com>
2024-10-18 14:25:19 +0200

    started home page for project site

---
+++ project/scripts/hamburger.js
@@ -0,0 +1,20 @@
+const mainnav = document.querySelector(".navigation");
+const hambutton = document.querySelector("#menu");
+
+hambutton.addEventListener("click", () => {
+    mainnav.classList.toggle("show");
+    hambutton.classList.toggle("show");
+});
+
+
+
+const currentPage = window.location.pathname.split("/").pop();
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+    const linkPage = link.getAttribute('href').split("/").pop();
+    if (linkPage === currentPage) {
+        link.classList.add('active');
+    }
+});
